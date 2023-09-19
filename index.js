76bf5390bc0b4543145d8ab2d17164941f1f9712
G A N Mahmud <gmahmud@testreach.com>
2023-09-20 00:58:17 +0600

    Fixed typos

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-modules.exports = function ({ addVariant, e, config }) {
+module.exports = function ({ addVariant, e, config }) {
     if (!config('theme.languages')) {
         config('theme.languages', ['bn']);
     }
     config('theme.languages').forEach((lang) => {
         addVariant(`lang-${lang}`, ({ modifySelectors, separator }) => {
             modifySelectors(({ className }) => {
-                return `[lang="${lang}"] .${e(`${lang}${separator}${className}`)}`;
+                return `[lang="${lang}"] .${e(`lang-${lang}${separator}${className}`)}`;
             });
         });
     });
